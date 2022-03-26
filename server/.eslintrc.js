01cafff945fe4aa2c9026dfc2aeaa729470ef514
GrandMaster5000 <ggamer556@mail.ru>
2022-03-26 22:04:43 +0300

    config eslint

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -2,12 +2,15 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
+    "eslint:recommended",
+		"plugin:@typescript-eslint/eslint-recommended",
+		"plugin:@typescript-eslint/recommended",
+		"plugin:prettier/recommended"
   ],
   root: true,
   env: {
@@ -16,9 +19,26 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-  },
+      "@typescript-eslint/no-empty-function": 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      "@typescript-eslint/explicit-function-return-type": [
+        "warn"
+      ],
+      "@typescript-eslint/no-unused-vars": [
+        "off"
+      ],
+      "@typescript-eslint/no-empty-interface": 'off',
+      "prettier/prettier": [
+        "error",
+        {
+          "singleQuote": true,
+          "useTabs": true,
+          "semi": true,
+          "trailingComma": "all",
+          "bracketSpacing": true,
+          "printWidth": 100,
+          "endOfLine": "auto"
+        }
+      ]
+    },
 };
